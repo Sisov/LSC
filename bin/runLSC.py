4e62cfb19e3c47a22ef06b11cd67a63a0a27625d
Jason L Weirather <jason.weirather@gmail.com>
2015-07-09 10:53:46 -0500

    Added finer control over parallelization of LSC.

--- bin/runLSC.py
+++ bin/runLSC.py
@@ -1,28 +1,22 @@
 #!/usr/bin/python
 
-import argparse
-import sys
-import os
-import json
-#from numpy import * # Where is this being used?
+import argparse, sys, os, json, subprocess
 import datetime
-from re import * # Where is this being used?
-from copy import * # Where is this being used?
-import threading #This should be changed to a Pool later
-import string
-import commands # This is used for running some line counts later and can probably go away when those do.
-from SequenceBasics import GenericFastaFileReader, GenericFastqFileReader
 from multiprocessing import cpu_count, Pool
-import subprocess
 from random import randint
+from shutil import copyfile
+from SequenceBasics import GenericFastaFileReader, GenericFastqFileReader
 from SequenceCompressionBasics import HomopolymerCompressionFactory
 from SamToNavBasics import SamToNavFactory
-from shutil import copyfile
 
-
-################################################################################
 
 def main():
+  # Run modes for runLSC.py are
+  # a. Mode 0: Run all the way through (Modes 1-3)
+  # b. Mode 1: Run the homopolymer compression to prepare short reads data.
+  # c. Mode 2: Run the alignment all the way through correction.
+  # c(alternative). Parallelized Mode 2: Run the alignment all the way through correction on a subset of long reads.
+  # d. Mode 3: Compile all the individual corrected reads from mode 2 into a single output.
   version = "2.alpha"
   parser = argparse.ArgumentParser(description="LSC "+version+": Correct errors (e.g. homopolymer errors) in long reads, using short read data",formatter_class=argparse.ArgumentDefaultsHelpFormatter)
   parser.add_argument('--long_reads',required=True,help="FASTAFILE Long reads to correct")
@@ -32,7 +26,9 @@ def main():
   group.add_argument('--tempdir',default='/tmp',help="FOLDERNAME where temporary files can be placed")
   group.add_argument('--specific_tempdir',help="FOLDERNAME of exactly where to place temproary folders")
   parser.add_argument('-o','--output',required=True,help="FOLDERNAME where output is to be written")
-  parser.add_argument('--mode',default=0,type=int,help="0: run through")
+  group = parser.add_mutually_exclusive_group()
+  group.add_argument('--mode',default=0,choices=[0,1,2,3],type=int,help="0: run through")
+  group.add_argument('--parallelized_mode_2',type=int,help="Mode 2, but you specify a sigle batch to execute.")
   parser.add_argument('--sort_mem_max',type=int,help="-S option for memory in unix sort")
   parser.add_argument('--minNumberofNonN',type=int,default=40,help="Minimum number of non-N characters in the compressed read")
   parser.add_argument('--maxN',type=int,help="Maximum number of Ns in the compressed read")
@@ -47,7 +43,11 @@ def main():
 
   # Establish running parameters here.  Some of these may need to become command line options.
   python_path = "/usr/bin/python"
-  mode = args.mode
+  mode = 0
+  if args.parallelized_mode_2:
+    mode = 2
+  elif args.mode:
+    mode = args.mode
   LR_pathfilename = args.long_reads
   SR_pathfilename = args.short_reads[0]
   SR_filetype = 'fa'
@@ -75,6 +75,9 @@ def main():
 
   if not os.path.isdir(output_foldername):
     os.makedirs(output_foldername)
+
+  if mode > 1 and not args.specific_tempdir:
+    sys.stderr.write("Error: to run mode 2 or 3 you need to specify a temporary directory with --specific_tempdir.  An easy way to create one is to run mode 1 with the --specific_tempdir option.\n")
 
   if not os.path.isdir(temp_foldername):
     if mode == 2:
@@ -143,10 +146,27 @@ def main():
 
   ######################################################################
   # Find the total number of batches we will run (regardless of run type)
-  sys.stderr.write("===batch count:===\n")    
-  total_batches = batch_count_LR(temp_foldername+'LR.fa',args.long_read_batch_size)
-  sys.stderr.write("Work will begin on "+str(total_batches)+" batches.\n")
-  sys.stderr.write(str(datetime.datetime.now()-t0)+"\n")
+  if mode == 0 or mode == 1 or mode == 2 and not args.parallelized_mode_2:
+    sys.stderr.write("===batch count:===\n")    
+    total_batches = batch_count_LR(temp_foldername+'LR.fa',args.long_read_batch_size)
+    of_batch = open(temp_foldername+'batch_count','w')
+    of_batch.write(str(total_batches)+"\n")
+    of_batch.close()
+    sys.stderr.write("Work will begin on "+str(total_batches)+" batches.\n")
+    sys.stderr.write(str(datetime.datetime.now()-t0)+"\n")
+  elif args.parallelized_mode_2:
+    total_batches = 0
+    with open(temp_foldername+"batch_count") as inf:
+      read_batches = int(inf.readline().rstrip())
+      if not read_batches:
+        sys.stderr.write("ERROR. problem reading batch count\n")
+        sys.exit()
+      total_batches = read_batches
+    if args.parallelized_mode_2 > 0 and args.parallelized_mode_2 <= total_batches:
+      sys.stderr.write("===Parallelized mode:===\nWill begin work on batch #"+str(args.parallelized_mode_2)+"\n")
+    else:
+      sys.stderr.write("ERROR invalid batch number for parallized_mode_2\n")
+      sys.exit()
 
   ##########################################
   # Compress the long reads
@@ -168,6 +188,17 @@ def main():
     of_corrected = open(temp_foldername+"corrected_LR.fa",'w')
     of_corrected_fq = open(temp_foldername+"corrected_LR.fq",'w')
     of_full = open(temp_foldername+"full_LR.fa",'w')
+    # read total batches from the file
+    if not os.path.isfile(temp_foldername+"batch_count"):
+      sys.stderr.write("ERROR you must run in modes 1 and 2 (or 0) before running mode 3\n")
+      sys.exit()
+    with open(temp_foldername+"batch_count") as inf:
+      read_batches = int(inf.readline().rstrip())
+      if not read_batches:
+        sys.stderr.write("ERROR. problem reading batch count\n")
+        sys.exit()
+      total_batches = read_batches
+    sys.stderr.write("Producing outputs from "+str(total_batches)+" batches\n")
     for i in range(1,total_batches+1):
       if not os.path.isfile(temp_foldername+"output."+str(i)+".file_uncorrected"):
         sys.stderr.write("Warning missing file "+temp_foldername+"output."+str(i)+".file_uncorrected"+"\n")
@@ -297,22 +328,60 @@ def batch_count_LR(lr_filename,batchsize):
     batch_number += 1
   return batch_number
 
+# This function launches our jobs (mode 2), and decides at which level (if any) to parallelize the job based on mode and thread counts.
+# Pre: long read files
+#      input arguments
+#      temporary folder name
+#      batch size
+# Post: For each batch of batch size that runs, the temporarly folder
+#       will get the results of LSC for that batch.  
+#       Files output are 
+#     output.#.file_uncorrected
+#     output.#.file_uncorrected_fq
+#     output.#.file_corrected
+#     output.#.file_full
+#       where # is the batch number
+# Modifies: Writes temporary files. Its tricky in how it runs.
+#   If you're using a single computer to run through and its not in the --parallelized_mode_2,
+#     it will run a Pool of jobs and give a single core to each job to run over the batch_size of long reads
+#   If you're using a single computer to run a parallelized_mode_2 job where you going to execute one of 
+#     the sets from the batches, it will use the number of threads you specify within that batch to speed processing
+#   The steps it runs through are better defined in execute_batch 
 def execute_LR(lr_filename,args,temp_foldername,batchsize):
   gfr = GenericFastaFileReader(lr_filename)
   batch = []
   batch_number = 0
+  if args.threads > 1 and not args.parallelized_mode_2: 
+    p = Pool(processes=args.threads)
   while True:
     entry = gfr.read_entry()
     if not entry: break
     batch.append([entry['name'],entry['seq']])
     if len(batch) >= batchsize:
       batch_number += 1
-      execute_batch(json.dumps(batch),batch_number,args.minNumberofNonN,args.maxN,temp_foldername,args.threads,args.error_rate_threshold,args.short_read_coverage_threshold,args.sort_mem_max)
+      # We will not use Pool if we are only using one thread or we are doing parallelized mode
+      if (not args.parallelized_mode_2 and args.threads <= 1) or args.parallelized_mode_2 == batch_number:
+        execute_batch(json.dumps(batch),batch_number,args.minNumberofNonN,args.maxN,temp_foldername,args.threads,args.error_rate_threshold,args.short_read_coverage_threshold,args.sort_mem_max)
+      # We will launch by Pool if we are only running on one system.
+      # In this case we only give each pool job one processor
+      elif not args.parallelized_mode_2:
+        p.apply_async(execute_batch,args=(json.dumps(batch),batch_number,args.minNumberofNonN,args.maxN,temp_foldername,1,args.error_rate_threshold,args.short_read_coverage_threshold,args.sort_mem_max))
       batch = []
+  #Handle the case were a buffered sequence(s) still needs run.
   if len(batch) > 0:
     batch_number += 1
-    execute_batch(json.dumps(batch),batch_number,args.minNumberofNonN,args.maxN,temp_foldername,args.threads,args.error_rate_threshold,args.short_read_coverage_threshold,args.sort_mem_max)
+    # We will not use Pool if we are only using one thread or we are doing parallelized mode
+    if (not args.parallelized_mode_2 and args.threads <= 1) or args.parallelized_mode_2 == batch_number:
+      execute_batch(json.dumps(batch),batch_number,args.minNumberofNonN,args.maxN,temp_foldername,args.threads,args.error_rate_threshold,args.short_read_coverage_threshold,args.sort_mem_max)
+    # We will launch by Pool if we are only running on one system.
+    # In this case we will only give each pool job one processor
+    elif not args.parallelized_mode_2:
+      p.apply_async(execute_batch,args=(json.dumps(batch),batch_number,args.minNumberofNonN,args.maxN,temp_foldername,1,args.error_rate_threshold,args.short_read_coverage_threshold,args.sort_mem_max))
   sys.stderr.write("\n")
+  #If we are running jobs as pools we clean up those threads here.
+  if args.threads > 1 and not args.parallized_mode_2:
+    p.close()
+    p.join()
   return batch_number
 
 def execute_batch(batch_json,batch_number,minNumberofNonN,maxN,temp_foldername,threads,error_rate_threshold,short_read_coverage_threshold,sort_max_mem):
@@ -339,6 +408,7 @@ def execute_batch(batch_json,batch_number,minNumberofNonN,maxN,temp_foldername,t
   subprocess.call(cmd1.split(),stderr=of_log,stdout=of_log)
   #step 3 map short reads
   sys.stderr.write("... executing batch "+str(batch_number)+".3 (alignment)   \r")
+  threads = 1
   cmd2 = 'hisat --reorder -f -p '+str(threads)+' -x '+temp_foldername+'LR.fa.'+str(batch_number)+'.cps.hisat-index -U '+temp_foldername+'SR.fa.cps -S '+temp_foldername+'LR.fa.'+str(batch_number)+'.cps.sam'
   subprocess.call(cmd2.split(),stderr=of_log,stdout=of_log)
   #step 4 convert sam to nav
